ce8e711bf7699d6b286e748046b024c9147d1092
Param Dwivedi <68422855+ParamDwivedi@users.noreply.github.com>
2024-08-30 23:23:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-let ques = '[QUESTION]';
+let ques = 'Am I a level 69420 rizzler?';
 
 random_num = Math.floor(Math.random() * 9);
 
-console.log('Question:',ques);
+console.log('Question:',ques)
 if (random_num === 1){
-  console.log("Yes - definitely.");
+  console.log("Yes - definitely.")
 } else if (random_num === 2){
-  console.log("It is decidedly so.");
+  console.log("It is decidedly so.")
 } else if (random_num === 3){
-  console.log("Without a doubt.");
+  console.log("Without a doubt.")
 } else if (random_num === 4){
-  console.log("Reply hazy, try again.");
+  console.log("Reply hazy, try again.")
 } else if (random_num === 5){
-  console.log("Ask again later.");
+  console.log("Ask again later.")
 } else if (random_num === 6){
-  console.log("Better not tell you now.");
+  console.log("Better not tell you now.")
 } else if (random_num === 7){
-  console.log("My sources say no.");
+  console.log("My sources say no.")
 } else if (random_num === 8){
-  console.log("Outlook not so good.");
+  console.log("Outlook not so good.")
 } else if (random_num === 9){
-  console.log("Very doubtful.");
+  console.log("Very doubtful.")
+} else{
+  console.log('You are not supposed to see this message 💀. Anyways, an error has occured, so please try again');
 }
